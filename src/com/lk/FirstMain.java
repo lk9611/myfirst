a41aff2160dcd47375acf5d6b7aa75224c97956a
lk9611 <31378543+lk9611@users.noreply.github.com>
2019-02-27 08:47:38 +0800

    Update FirstMain.java

--- src/com/lk/FirstMain.java
+++ src/com/lk/FirstMain.java
@@ -3,7 +3,7 @@ package com.lk;
 public class FirstMain {
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        System.out.println("hello world33333");
 
         System.out.println("dddd");
 
